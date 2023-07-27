fc388199ccdd3a41e7e029cfa6a5fdf031d6c613
Tijesunimi Peters <tijesunimipeters+github@gmail.com>
2023-07-27 12:21:33 -0500

    Create camundaLayersJson.js

---
+++ camundaLayersJson.js
@@ -0,0 +1,44 @@
+let something = [
+    {
+        "layer": 1,
+        "ti": 3,
+        "packInstructions": [
+            {
+                "acceptableCases": [],
+                "caseLocationId": "CWC02PACK",
+                "buildSequence": 1,
+                "layer": 1,
+                "inventoryId": null,
+                "palletPlacement": null,
+                "caseNumber": 1
+            },
+            {
+                "acceptableCases": [],
+                "caseLocationId": "CWC02PACK",
+                "buildSequence": 2,
+                "layer": 1,
+                "inventoryId": null,
+                "palletPlacement": null,
+                "caseNumber": 2
+            },
+            {
+                "acceptableCases": [],
+                "caseLocationId": "CWC02PACK",
+                "buildSequence": 3,
+                "layer": 1,
+                "inventoryId": null,
+                "palletPlacement": null,
+                "caseNumber": 2
+            },
+            {
+                "acceptableCases": [],
+                "caseLocationId": "CWC02PACK",
+                "buildSequence": 3,
+                "layer": 1,
+                "inventoryId": null,
+                "palletPlacement": null,
+                "caseNumber": 3
+            }
+        ]
+    }
+]
